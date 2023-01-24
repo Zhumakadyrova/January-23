8ee4884193a6a18034f4c178517f8f985b216894
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-01-24 16:26:19 +0600

    task about week and weekend days

--- Новая папка/index.js
+++ Новая папка/index.js
@@ -60,10 +60,64 @@ let ex6 = 6**6;
 let ex7 = 50%3;
 console.log (ex7);
 
+let number = "five";
 
+if (number > 0){
+  console.log ("Number " + number + " positive");
+}
+else if (number < 0) {
+  console.log ("Number" + number + "negative");
+}
+else if (number === 0){
+  console.log ("Number zero is not negative or positive");
+}
+else {
+  console.log (number + " This is not a number");
+}
 
+let number1 = "km";
+if (number1 % 2 === 0) {
+  console.log ( number1 + " even");
+}
+else if (number1 % 2 === 1) {
+  console.log ( number1 + " odd");
+}
+else {
+  console.log (number1 + " Wrong comand");
+}
+let money = "knn";
+let price = 40;
+if (price <= money) {
+  console.log (money + "is enough");
+}
+else if (money < price) {
+  console.log (money + "is not enough");
+}
+else {
+  console.log (money + " wrong comand");
+}
 
-
-
-
+let month = "january";
+if (month === "december" || month === "january" || month === "february") {
+  console.log ( month + " this is winter");
+}
+else if (month === "march" || month === "april" || month === "may") {
+  console.log ( month + " this is spring");
+}
+else if (month === "june" || month === "july" || month === "august") {
+  console.log ( month + " this is summer");
+}
+else if (month === "september" || month === "october" || month === "november") {
+  console.log ( month + " this is autumn");
+}
+else {
+  console.log (month + " this is wrong comand");
+}
+let day = "friday";
+if (day === "monday" || day === "tuesday" || day === "wednesday" || day === "thursday"  || day === "friday") {
+  console.log ( day + " this is a week day");
+}
+else if (day === "saturday" || day === "sunday") {
+  console.log ( day + " this is a weekend day");
+}
 
