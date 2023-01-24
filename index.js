2d53ec23eec9637bbc519ecec2ac4beb88f16cdb
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-01-24 15:03:27 +0600

    begin to work with if else

---
+++ index.js
@@ -0,0 +1,15 @@
+let heater = 2.4;
+let time = 46;
+let perHour = 2.44;
+let total = (perHour * time) * heater;
+
+console.log ("за месяц = " + total)
+
+let height = 23;
+let width = 33;
+let length = 50;
+let weight = 12;
+let pricePerKg = 499;
+let total1 = 499*12;
+
+console.log ("price = " + total1)
---
+++ Новая папка/index.js
@@ -0,0 +1,69 @@
+let myName = "Nagima";
+console.log (myName);
+
+let myTown = "Karakol";
+let myHobby = "Books";
+let myFavoriteColor = "white";
+let myCountry = "Kyrgyzstan";
+let myStatus = "Student";
+let course = "IT"
+let project = "Girls in IT";
+console.log (project);
+let total = "17 students";
+let myDream = "To be best in my major";
+console.log (myDream);
+let favoriteActivity = "Hiking";
+console.log (favoriteActivity);
+
+let myAge = 21;
+let myBirthday = 07;
+let myMonth = 11;
+let myYear = 2001;
+let myFavoiteNumber = 9;
+let KarakolPostcode = 722200;
+let population = 7000000;
+let familyMembers = 6;
+let students = 25;
+let totalAmount = 546000;
+let weight = 67;
+
+let student = "male";
+if( student = "male") {
+  console.log ("It is male");
+}
+let lightSwitchIsOn = "Switch is on";
+if (lightSwitchIsOn) {
+  console.log("Switch is on");
+}
+else {
+  console.log("Switch is off");
+}
+
+let height = 9;
+let width = 10;
+let square = height * width;
+console.log (square);
+
+let girl = 50;
+let boy = 50;
+let amount = 50 + 50;
+let percent = (50*50)/100;
+console.log (percent);
+
+let ex = (5 + 1) >= 6;
+let ex1 = 6 ==6;
+let ex2 = 10 !== 11;
+let ex4 =10 > 6;
+let ex5 = 15 < 20;
+
+let ex6 = 6**6;
+let ex7 = 50%3;
+console.log (ex7);
+
+
+
+
+
+
+
+
