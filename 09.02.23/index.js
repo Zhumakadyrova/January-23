0389ef7cdbb2ad4bf0e95e7c96dfb3c2ddaefa1b
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-02-09 13:04:00 +0600

    practice array

--- 09.02.23/index.js
+++ 09.02.23/index.js
@@ -3,3 +3,20 @@ while (num <= 100) {
   console.log(num)
   num +=10
 }
+let num1 = 5 
+while (num1 >= -15){
+  console.log(num1)
+  num1 -= 3
+}
+
+for(let a= 15; a >= 10; a--) {
+  console.log(a);
+}
+
+let fruits = ["apple", "pear", "orange"];
+console.log(fruits);
+fruits.push("cherry");
+console.log(fruits);
+fruits.unshift("appricote");
+console.log(fruits);
+
